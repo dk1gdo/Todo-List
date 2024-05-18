78dc89de1ae4d42de86c19bdacb686bf8a2a7bc5
dk1gdo <d.garieff@yandex.ru>
2024-05-18 07:42:18 +0500

    First version

--- Todo List/ContentView.swift
+++ Todo List/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var currentTodo = ""
+    @State private var todos: [Item] = []
+    
+    private func save() {
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(self.todos), forKey: "myTodosKey")
+    }
+    
+    private func load() {
+        if let todosData = UserDefaults.standard.value(forKey: "myTodosKey") as? Data {
+            if let todoList = try? PropertyListDecoder().decode(Array<Item>.self, from: todosData){
+                self.todos = todoList
+            }
         }
-        .padding()
+    }
+    
+    private func delete(at offset: IndexSet){
+        self.todos.remove(atOffsets: offset)
+        save()
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("New todo..", text: $currentTodo)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: {
+                        guard !self.currentTodo.isEmpty else { return }
+                        self.todos.append(Item(todo: self.currentTodo))
+                        self.currentTodo = ""
+                        self.save()
+                    }){
+                        Image(systemName: "text.badge.plus")
+                    }
+                    .padding(.leading, 5)
+                }.padding()
+                List {
+                    ForEach(todos) { todoEntity in
+                        HStack{
+                            Text("\(todoEntity.todo)")
+                        }
+                    }.onDelete(perform: delete)
+                }
+            }.navigationTitle("Todo List")
+        }.onAppear(perform: {
+            self.load()
+        })
+        
     }
 }
 
--- Todo List/Item.swift
+++ Todo List/Item.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct Item: Identifiable, Codable {
+    var id = UUID()
+    var todo: String
+}
